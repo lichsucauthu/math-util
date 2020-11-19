0afbc1467351d62611e315f0e775828e4d5017f1
lichsucauthu <anhnguyentien3006@gmail.com>
2020-11-19 08:19:58 +0700

    CI with first GREEN

--- test/anh/util/test/MathUtilityTest.java
+++ test/anh/util/test/MathUtilityTest.java
@@ -36,6 +36,7 @@ public class MathUtilityTest {
         assertEquals(6, MathUtility.getFactorial(3));
         assertEquals(1, MathUtility.getFactorial(0));
         assertEquals(1, MathUtility.getFactorial(1));
+        assertEquals(1, MathUtility.getFactorial(6));//toi muon thay xanh o trên git
         //mac dinh code test doc lap voi code chinh
         //doc lap voi qua trinh clean and build 
         //dung cod ban xanh hay do mien la error cu phap ban luon dong goi hay build ra dc file .jar .war
